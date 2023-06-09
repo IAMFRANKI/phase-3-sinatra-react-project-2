992a8b4911d911f259a9f1b83cec30cd2c34e5a1
frank <thmsfrank04@gmail.com>
2023-06-08 19:44:32 -0700

    Back end update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+User.destroy_all
+Todo.destroy_all
+
+5.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+  )
+end
+
 
 puts "✅ Done seeding!"
